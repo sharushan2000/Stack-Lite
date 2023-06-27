fbe18f33ae56ffcdb6a6b8e2c8a7f88a2b0014ad
sharushan2000 <sharushan0000@gmai.com>
2023-06-27 14:41:57 -0400

    trusted host updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY =env('DJANGO_SECRET_KEY')
 DEBUG =env('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1",'stacklite.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ["https://stacklite.up.railway.app/",
-
-]
+CSRF_TRUSTED_ORIGINS = ['https://stacklite.up.railway.app',]
 
 
 # Application definition
