0c2341bab7e3f39d84ac4cf0bfc6734c00699ba7
sharushan2000 <sharushan0000@gmai.com>
2023-11-07 20:29:26 -0500

    chhange allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY =env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["*",]
-CSRF_TRUSTED_ORIGINS = ['https://',]
+ALLOWED_HOSTS = ["localhost","127.0.0.1" ,"web-production-5536.up.railway.app","https://stacklite.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ['https://stacklite.up.railway.app/',]
 
 
 # Application definition
